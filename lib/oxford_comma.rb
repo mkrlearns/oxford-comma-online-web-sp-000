e91fcb41bf15bf6599270c147b6054a827cf96f5
ryan m <contact@ryanmeek.com>
2019-03-05 19:58:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   string = ""
   array.each do |item|
-    if item.length == 2 && item == item.first
-      string << item.length
-    elsif item != array.last && item.length > 2
-      string << "#{item.length}, "
+    if array.length == 2 && item == item.first
+      string << item
+    elsif item != array.last && array.length > 2
+      string << "#{item}, "
     else
-      string << "and #{item.length}"
+      string << "and #{item}"
     end
   end
   string
